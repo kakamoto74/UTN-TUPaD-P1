0884864db07ede48e92243e2c44a99adda2fe426
kakamoto74 <cgomez074@gmail.com>
2025-05-17 11:09:06 -0300

    Add files via upload

--- 04 Estructuras Repetitivas/GÓMEZ-CRISTIAN_TP-04.py
+++ 04 Estructuras Repetitivas/GÓMEZ-CRISTIAN_TP-04.py
@@ -1,4 +1,4 @@
-# TP 4 Estructuras Repetitivas - Pablo Garay
+# TP 4 Estructuras Repetitivas - Cristian Gómez
 
 # 1 - Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100 (incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
 for x in range(101):
